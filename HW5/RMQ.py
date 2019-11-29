9fbf8860ea2527291b969b2de43b66a6c9527d29
FergusPrimus <fergusprimus@gmail.com>
2019-11-29 10:52:24 -0700

    first pass on RMQ with help from https://www.youtube.com/watch?v=c5O7E_PDO4U

--- HW5/RMQ.py
+++ HW5/RMQ.py
@@ -5,11 +5,30 @@ import sys
 n = 100
 rand_arr = np.random.randint(n+1, size=n)
 sort_arr = np.sort(np.random.randint(n+1, size=n))
-table = np.zeros((n, math.floor(math.log(n))+1), dtype=int)
+sparse = np.zeros((n, math.floor(math.log(n))+1), dtype=int)
+
+
+def preprocess(input):
+
+    for i in range(n):
+        sparse[i][0] = i
+    j = 1
+    i = 0
+    while 2**j <= n:
+        while i+(2**j)-1 < n:
+            if input[sparse[i][j-1]] < input[sparse[i+2**(j-1)][j-1]]:
+                sparse[i][j] = sparse[i][j-1]
+            else:
+                sparse[i][j] = sparse[i+2**(j-1)][j-1]
+            i += 1
+        j += 1
+    return sparse
+
 
 def rangeMinimumQuerry():
     print(rand_arr)
     print(sort_arr)
 
 
+print(preprocess(rand_arr))
 rangeMinimumQuerry()
